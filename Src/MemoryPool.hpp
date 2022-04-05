4959b15db15b62ad8d940f915b7fd3f53af01589
Skykey <zcxzxlc@163.com>
2022-04-05 19:38:08 +0800

    OK

---
+++ Src/MemoryPool.hpp
@@ -0,0 +1,107 @@
+﻿#ifndef MEMORYPOOL_MEMORYPOOL_HPP
+#define MEMORYPOOL_MEMORYPOOL_HPP
+
+#include <climits>
+#include <cstddef>
+
+namespace Diana {
+
+	template<typename T, size_t BlockSize = 4096>
+	class MemoryPool {
+	public:
+		typedef T* pointer;
+
+		// rebind<U>::other
+		template<typename U>
+		struct rebind {
+			typedef MemoryPool<U> other;
+		};
+
+		MemoryPool() noexcept {
+			currentBlock_ = nullptr;
+			currentSlot_ = nullptr;
+			lastSlot_ = nullptr;
+			freeSlot_ = nullptr;
+		}
+
+		~MemoryPool() noexcept {
+			// 循环销毁内存池中分配的内存区块
+			slot_pointer_ curr = currentBlock_;
+			while (curr != nullptr) {
+				slot_pointer_ prev = curr->next;
+				operator delete(reinterpret_cast<void*>(curr));
+				curr = prev;
+			}
+		}
+
+		pointer allocate(size_t = 1, const T* hint = 0) {
+			// 如果有空闲的对象槽，直接将空闲区域交付出去
+			if (freeSlot_ != nullptr) {
+				pointer result = reinterpret_cast<pointer>(freeSlot_);
+				freeSlot_ = freeSlot_->next;
+				return result;
+			} else {
+				// 若对象槽不够用，分配一个新的内存区块
+				if (currentSlot_ >= lastSlot_) {
+					data_pointer_ newBlock = reinterpret_cast<data_pointer_>(operator new(BlockSize));
+					reinterpret_cast<slot_pointer_>(newBlock)->next = currentBlock_;
+					currentBlock_ = reinterpret_cast<slot_pointer_>(newBlock);
+					// 填补整个区块来满足元素内存区域对齐的要求
+					data_pointer_ body = newBlock + sizeof(slot_pointer_);
+					uintptr_t result = reinterpret_cast<uintptr_t>(body);
+					size_t bodyPadding = (alignof(slot_type_) - result) % (alignof(slot_type_));
+					currentSlot_ = reinterpret_cast<slot_pointer_>(body + bodyPadding);
+					lastSlot_ = reinterpret_cast<slot_pointer_>(newBlock + BlockSize - sizeof(slot_type_) + 1);
+				}
+				return reinterpret_cast<pointer>(currentSlot_++);
+			}
+		}
+
+		void deallocate(pointer p, size_t n = 1) {
+			if (p != nullptr) {
+				reinterpret_cast<slot_pointer_>(p)->next = freeSlot_;
+				freeSlot_ = reinterpret_cast<slot_pointer_>(p);
+			}
+		}
+
+		template<typename U, typename... Args>
+		void construct(U* p, Args&&... args) {
+			new (p) U(std::forward<Args>(args)...);// 折叠表达式 C++17
+		}
+
+		template<typename U>
+		void destroy(U* p) {
+			p->~U();
+		}
+
+	private:
+		// 存储内存池中的对象槽
+		union Slot_ {
+			T element;
+			Slot_* next;
+		};
+
+		// 数据指针
+		typedef char* data_pointer_;
+		// 对象槽
+		typedef Slot_ slot_type_;
+		// 对象槽指针
+		typedef Slot_* slot_pointer_;
+
+		// 指向当前内存区块
+		slot_pointer_ currentBlock_;
+		// 指向当前内存区块的一个对象槽
+		slot_pointer_ currentSlot_;
+		// 指向当前内存区块的最后一个对象槽
+		slot_pointer_ lastSlot_;
+		// 指向当前内存区块的空闲对象槽
+		slot_pointer_ freeSlot_;
+
+		// 检查定义的内存池大小是否过小
+		static_assert(BlockSize >= 2 * sizeof(slot_type_), "Block size too small.");
+	};
+
+};// namespace Diana
+
+
+#endif//MEMORYPOOL_MEMORYPOOL_HPP
---
+++ Src/StackAlloc.hpp
@@ -0,0 +1,73 @@
+﻿#ifndef MEMORYPOOL_STACKALLOC_HPP
+#define MEMORYPOOL_STACKALLOC_HPP
+
+#include <memory>
+
+namespace Diana {
+	template<typename T>
+	struct StackNode_ {
+		T data;
+		StackNode_* prev;
+	};
+
+	// T为储存的对象类型，Alloc为使用的分配器（默认使用std::allocator）
+	template<typename T, typename Alloc = std::allocator<T>>
+	class StackAlloc {
+	public:
+		typedef StackNode_<T> Node;
+		typedef typename Alloc::template rebind<Node>::other allocator;
+
+		StackAlloc() { head_ = 0; }
+		~StackAlloc() { clear(); }
+
+		// 当栈中元素为空时返回true
+		bool empty() { return (head_ == 0); };
+
+		// 释放栈中所有元素
+		void clear() {
+			Node* curr = head_;
+			// 依次出栈
+			while (curr != 0) {
+				Node* tmp = curr->prev;
+				allocator_.destroy(curr);
+				allocator_.deallocate(curr, 1);
+				curr = tmp;
+			}
+			head_ = 0;
+		}
+
+		// 压栈
+		void push(T element) {
+			// 为一个节点分配内存
+			Node* newNode = allocator_.allocate(1);
+			// 调用节点的构造函数
+			allocator_.construct(newNode, Node());
+
+			// 入栈操作
+			newNode->data = element;
+			newNode->prev = head_;
+			head_ = newNode;
+		}
+
+		// 出栈
+		T pop() {
+			// 出栈返回元素
+			T result = head_->data;
+			Node* tmp = head_->prev;
+			allocator_.destroy(head_);
+			allocator_.deallocate(head_, 1);
+			head_ = tmp;
+			return result;
+		}
+
+		// 返回栈顶元素
+		T top() { return head_->data; }
+
+	private:
+		allocator allocator_;
+		Node* head_;
+	};
+
+};// namespace Diana
+
+#endif//MEMORYPOOL_STACKALLOC_HPP
